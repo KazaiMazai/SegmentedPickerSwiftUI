e40f2cbd0f5461818012bb564ca04de0e12745c4
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-15 18:46:44 +0100

    minor fix

--- Tests/SegmentedPickerTests/CapsuleSelectionViewTests.swift
+++ Tests/SegmentedPickerTests/CapsuleSelectionViewTests.swift
@@ -13,11 +13,11 @@ import SnapshotTesting
 final class CapsuleSelectionViewTests: XCTestCase {
     func test_WhenSelectionIsNotProvided_SelectionIsNotVisible_Snapshot() {
         let view = CapsuleSelectionView(titles: ["One", "Two", "Three"])
-        assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13Pro))])
+        assertSnapshots(of: view, as: [Snapshotting<CapsuleSelectionView, UIImage>.image(layout: .device(config: .iPhone13Pro))])
     }
     
     func test_WhenSelectionIsProvided_SelectionIsVisible_Snapshot() {
         let view = CapsuleSelectionView(titles: ["One", "Two", "Three"], selectedIndex: 1)
-        assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13Pro))])
+        assertSnapshots(of: view, as: [Snapshotting<CapsuleSelectionView, UIImage>.image(layout: .device(config: .iPhone13Pro))])
     }
 }
