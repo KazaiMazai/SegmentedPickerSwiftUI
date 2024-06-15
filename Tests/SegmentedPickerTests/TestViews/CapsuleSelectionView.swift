aecb69d85209c3b165c1453b60dd47eefb5a0ad5
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-15 17:34:37 +0100

    added views for testing

---
+++ Tests/SegmentedPickerTests/TestViews/CapsuleSelectionView.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 15/06/2024.
+//
+
+import SwiftUI
+@testable import SegmentedPicker
+
+struct CapsuleSelectionView: View {
+   let titles: [String]
+       @State var selectedIndex: Int?
+
+   var body: some View {
+       VStack {
+           SegmentedPicker(
+               titles,
+               selectedIndex: Binding(
+                   get: { selectedIndex },
+                   set: { selectedIndex = $0 }),
+               content: { item, isSelected in
+                   Text(item)
+                       .foregroundColor(isSelected ? Color.white : Color.gray )
+                       .padding(.horizontal, 16)
+                       .padding(.vertical, 8)
+               },
+               selection: {
+                   Capsule()
+                       .fill(Color.gray)
+               })
+       }
+       
+       Spacer()
+   }
+}
