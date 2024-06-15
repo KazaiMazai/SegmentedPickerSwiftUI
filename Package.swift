f86e4697d890daac8d2f8a750fbd4e0ccfa78cb4
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-15 17:34:19 +0100

    added snapshot library

--- Package.swift
+++ Package.swift
@@ -15,8 +15,11 @@ let package = Package(
             targets: ["SegmentedPicker"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/pointfreeco/swift-snapshot-testing",
+            from: "1.12.0"
+        )
+       
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +29,9 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "SegmentedPickerTests",
-            dependencies: ["SegmentedPicker"]),
+            dependencies: [
+                "SegmentedPicker",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ]),
     ]
 )
