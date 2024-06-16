dda76f54eb340b0e10533d1d300372c5aeacaeea
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-16 20:04:56 +0100

    added compile directive to snapshot tests due to SnapshotTesting lib limitations

--- Tests/SegmentedPickerTests/UnderlineSelectionViewTests.swift
+++ Tests/SegmentedPickerTests/UnderlineSelectionViewTests.swift
@@ -4,14 +4,19 @@
 //
 //  Created by Sergey Kazakov on 15/06/2024.
 //
-
 import SwiftUI
 import UIKit
 import XCTest
 import SnapshotTesting
 @testable import SegmentedPicker
 
+extension CGSize {
+    static let testView: CGSize = CGSize(width: 390, height: 844)
+}
+
+#if os(iOS) || os(tvOS)
 final class UnderlineSelectionViewTest: XCTestCase {
+    
     func test_WhenSelectionIsNotProvided_SelectionIsNotVisible_Snapshot() {
         let view = UnderlineSelectionView(titles: ["One", "Two", "Three"])
         assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13))])
@@ -22,3 +27,4 @@ final class UnderlineSelectionViewTest: XCTestCase {
         assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13))])
     }
 }
+#endif
