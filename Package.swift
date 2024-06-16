3eab5b7b1cb422efb8de4cfe814237fa59cf5c13
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-16 14:46:02 +0100

    package.swift clean up

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
            .macOS(.v10_15)
        ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SegmentedPicker",
             targets: ["SegmentedPicker"]),
@@ -23,8 +22,6 @@ let package = Package(
        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentedPicker",
             dependencies: []),
