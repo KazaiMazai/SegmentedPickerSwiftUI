7f328cbd48e3642dee521f3b1d531f452a83dc03
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-16 14:46:13 +0100

    changed snapshot device

--- Tests/SegmentedPickerTests/UnderlineSelectionViewTests.swift
+++ Tests/SegmentedPickerTests/UnderlineSelectionViewTests.swift
@@ -14,11 +14,11 @@ import SnapshotTesting
 final class UnderlineSelectionViewTest: XCTestCase {
     func test_WhenSelectionIsNotProvided_SelectionIsNotVisible_Snapshot() {
         let view = UnderlineSelectionView(titles: ["One", "Two", "Three"])
-        assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13Pro))])
+        assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13))])
     }
     
     func test_WhenSelectionIsProvided_SelectionIsVisible_Snapshot() {
         let view = UnderlineSelectionView(titles: ["One", "Two", "Three"], selectedIndex: 1)
-        assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13Pro))])
+        assertSnapshots(of: view, as: [.image(layout: .device(config: .iPhone13))])
     }
 }
