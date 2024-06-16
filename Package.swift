1b225b4f25f2cbc36a6a8259f44ded109735ca53
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-16 16:33:26 +0100

    Added snapshot tests Github Action workflow (#11)
    * Added test workflow
    
    * removed swiftlint for a while
    
    * Fixed schema name
    
    * added macOS min version
    
    * minor fix
    
    * tests fix
    
    * Fix github action workflow
    
    * Update swift.yml
    
    * Update swift.yml
    
    * package.swift clean up
    
    * changed snapshot device
    
    * regenerated snapshots
    
    * Update swift.yml
    
    * Rename swift.yml to tests.yml

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SegmentedPicker",
     platforms: [
-           .iOS(.v13)
+           .iOS(.v13),
+           .macOS(.v10_15)
        ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SegmentedPicker",
             targets: ["SegmentedPicker"]),
@@ -22,8 +22,6 @@ let package = Package(
        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentedPicker",
             dependencies: []),
